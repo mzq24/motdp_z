0435a5e09a8a29343d4e9d7d5e245d779c9b6b75
mzq <mzq@example.com>
2026-03-17 17:02:26 +0800

    add stats bash file

---
+++ bridge_baseline/scripts/stats.sh
@@ -0,0 +1,6 @@
+cd /media/z/data/mzq/others/MoT-DP
+
+python bridge_baseline/scripts/compute_route_stats.py \
+    --dataset_path /media/z/data/dataset/pdm_lite_mini/train \
+    --key all \
+    --output_yaml bridge_baseline/bd_config_test.yaml
